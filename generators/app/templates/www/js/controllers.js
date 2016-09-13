3c4ce83e92d873aa293edf85fa132444a5f41622
Tejas <tejas@Tejass-MacBook-Pro.local>
2016-09-13 10:49:39 -0400

    cleanup

--- generators/app/templates/www/js/controllers.js
+++ generators/app/templates/www/js/controllers.js
@@ -1,18 +1,9 @@
 angular.module('starter.controllers', ['kinvey', 'ngCordova'])
 
-.controller('DashCtrl', function($scope) {
-
-    $scope.myTest = function() {
-        console.log('inside myTest');
-        $ionicSideMenuDelegate.toggleLeft();
-    };
-})
-
-
 .controller('MenuCtrl', function($scope, $kinvey, $ionicSideMenuDelegate, $ionicModal) {
     console.log('inside menuctrl');
+
     $scope.toggleLeft = function() {
-        console.log('inside toggleleft');
         $ionicSideMenuDelegate.toggleLeft();
     };
 
@@ -26,22 +17,8 @@ angular.module('starter.controllers', ['kinvey', 'ngCordova'])
 .controller('HomeCtrl', function($scope, $kinvey, $ionicSideMenuDelegate, $rootScope, $state) {
     console.log('home');
 
-    try {
-        navigator.geolocation.getCurrentPosition(function(loc) {
-            console.log('getting position');
-            var coord = [loc.coords.latitude, loc.coords.longitude];
-            console.log(coord);
-            $rootScope.current_loc = coord;
-        });
-    } catch (evt) {
-        alert('fail' + evt.message);
-    }
-
-
     $scope.$on('$ionicView.beforeEnter', function() {
         // we're authenticated, grab logo and color scheme
-        console.log('home');
-        //var activeUser = $kinvey.User.getActiveUser();
         var activeUser = $rootScope.getActiveUser();
 
         if (!activeUser) {
@@ -50,7 +27,7 @@ angular.module('starter.controllers', ['kinvey', 'ngCordova'])
         }
         
         $rootScope.primarycolor = "#D44B2B";
-        $rootScope.logo = "img/pharma_background.jpeg";
+        $rootScope.logo = "img/background.jpeg";
         $rootScope.textColor = "#D44B2B";
         $scope.$digest();
     });
